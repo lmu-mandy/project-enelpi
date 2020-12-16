724687ade0956b74f23493c308802719b70877e7
Luis Garcia <lgarci81@lion.lmu.edu>
2020-12-15 20:59:01 -0800

    comments this preprocessing file

--- corpus/parallel_parser.py
+++ corpus/parallel_parser.py
@@ -1,3 +1,8 @@
+"""
+This program can process the xml files in Christos Christodoulopoulos's bible-corpus
+repository and put matching verses of the different bible translations into a single
+row of a csv file as well as the verse identifier. 
+"""
 from bs4 import BeautifulSoup as bs
 import csv
 import os
@@ -5,6 +10,9 @@ import re
 
 
 def parse_bible(path):
+    """
+    Reads the contents of a bible xml file and returns it as a beutiful soup object
+    """
     content = []
     with open(path, "r") as file:
         content = file.read()
@@ -13,20 +21,32 @@ def parse_bible(path):
 
 
 def get_verses(bible):
+    """
+        Will return all the xml tags that are of type verse for a given bible file
+    """
     return bible.find_all("seg", {"type": "verse"})
 
 
 def get_ids_in_common(b1, b2):
+    """
+    Will find the verses that two bible translations have in common. 
+    """
     b1_ids = set([verse.get("id") for verse in b1])
     b2_ids = set([verse.get("id") for verse in b2])
     return b1_ids.intersection(b2_ids)
 
 
 def get_verse(bible, id):
+    """
+    Gets the verse of the given verse id for a given bible.
+    """
     return re.sub('[\\n\\t]', '', bible.find(id=id).text)
 
 
 def write_parallel_bible(bible1, bible2, writepath):
+    """
+    Writes the csv file with translations as rows. 
+    """
     verses_b1 = get_verses(bible1)
     verses_b2 = get_verses(bible2)
     verse_ids = get_ids_in_common(verses_b1, verses_b2)
